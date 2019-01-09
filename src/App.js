54af68368c22e924386c6329afa5588078d8692b
Guilherme Henrique <guilhermeerba@hotmail.com>
2019-01-09 21:15:15 -0200

    Navbar Added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import Nav from './Nav';
 
 class App extends Component {
-
   render() {
     return (
+      <div className="App">
+        <Nav />
 
+        <div className="container">
+        <div className="card card-body">
+        <h1>Search</h1>
+        <p className="lead">Digite um Nome</p>
+        <input onChange={this.getUser} id="search" type="text" className="form-control" required />
+        </div>
+        </div>
+
+      </div>
     );
   }
 }

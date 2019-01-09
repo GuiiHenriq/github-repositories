512e9cab61b697fd4180e68d717f45ae9868aae2
Guilherme Henrique <guilhermeerba@hotmail.com>
2019-01-09 21:13:56 -0200

    Built-in File Structure

---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nav = () => (
+  <nav className="navbar navbar-dark bg-info mb-3">
+  <div className="container">
+    <a href="#" className="navbar-brand">GitHub Repositories</a>
+    <p className="navbar-brand">Guilherme Henrique</p>
+  </div>
+  </nav>
+);
+
+export default Nav;

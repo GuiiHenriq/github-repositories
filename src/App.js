b7f8aa7c9abc0fd005819fa439cdb201c41fbfbb
Guilherme Henrique <guilhermeerba@hotmail.com>
2019-01-09 21:16:00 -0200

    GitHub API Added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 import Nav from './Nav';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      github: {
+        url: "https://api.github.com/users",
+        client_id: "e0774b4fc4223fe4370a",
+        client_secret: "7a107581f13d49c58f7919e103d010aad9e17db5",
+        count: 7,
+        sort: "created: asc"
+      },
+      user: [],
+      repos: []
+    }
+  }
+
+  getUser = (e) => {
+    
+  }
   render() {
     return (
       <div className="App">
